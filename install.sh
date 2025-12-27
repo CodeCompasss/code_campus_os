13ab49be7feb5249123878ec544d6a238d7824bc
shadil-rayyan <shadilrayyan2@gmail.com>
2025-12-27 16:37:27 +0530

    updated teh command to also install apps

---
+++ install.sh
@@ -0,0 +1,6 @@
+find "install" -type f -name "*.sh" -exec bash "{}" \;
+
+bash "custom root/shell.sh"
+
+bash "custom root/test.sh"
+
---
+++ install/desktop/app-gnome-extenstion-manger.sh
@@ -0,0 +1,4 @@
+sudo apt install -y \
+  gnome-shell-extension-manager \
+  gir1.2-gtop-2.0 \
+  gir1.2-clutter-1.0
