e649ec2bd7a7d982f9cf9513aa6123f012a1adeb
shadil-rayyan <shadilrayyan2@gmail.com>
2025-12-31 00:30:54 +0530

    updated the gnome.sh file

--- custom_root/gnome.sh
+++ custom_root/gnome.sh
@@ -2,10 +2,11 @@
 set -e
 
 SCHEMA_DIR="/usr/share/glib-2.0/schemas"
-OVERRIDE_FILE="$SCHEMA_DIR/90_extensions.gschema.override"
+OVERRIDE_FILE="$SCHEMA_DIR/99-extensions.gschema.override"
 
-cat > "$OVERRIDE_FILE" <<'EOF'
+sudo tee "$OVERRIDE_FILE" > /dev/null <<'EOF'
 [org.gnome.shell]
+disable-user-extensions=false
 enabled-extensions=[
   'add-to-desktop@tommimon.github.com',
   'arcmenu@arcmenu.com',
@@ -19,5 +20,4 @@ enabled-extensions=[
 ]
 EOF
 
-# Compile schemas (required inside ISO)
-glib-compile-schemas "$SCHEMA_DIR" || true
+sudo glib-compile-schemas "$SCHEMA_DIR"
