9cb1ccfab946aff81e1c0badddfa6dd1f0cc16ee
shadil-rayyan <shadilrayyan2@gmail.com>
2025-12-31 00:54:45 +0530

    updated the script again

--- custom_root/gnome.sh
+++ custom_root/gnome.sh
@@ -7,17 +7,7 @@ OVERRIDE_FILE="$SCHEMA_DIR/99-extensions.gschema.override"
 sudo tee "$OVERRIDE_FILE" > /dev/null <<'EOF'
 [org.gnome.shell]
 disable-user-extensions=false
-enabled-extensions=[
-  'add-to-desktop@tommimon.github.com',
-  'arcmenu@arcmenu.com',
-  'caffeine@patapon.info',
-  'clipboard-indicator@tudmotu.com',
-  'dash-to-panel@jderose9.github.com',
-  'screenshot-window-sizer@gnome-shell-extensions.gcampax.github.com',
-  'tiling-assistant@ubuntu.com',
-  'Vitals@CoreCoding.com',
-  'blur-my-shell@aunetx'
-]
+enabled-extensions=['add-to-desktop@tommimon.github.com','arcmenu@arcmenu.com','caffeine@patapon.info','clipboard-indicator@tudmotu.com','dash-to-panel@jderose9.github.com','screenshot-window-sizer@gnome-shell-extensions.gcampax.github.com','tiling-assistant@ubuntu.com','Vitals@CoreCoding.com','blur-my-shell@aunetx']
 EOF
 
 sudo glib-compile-schemas "$SCHEMA_DIR"
